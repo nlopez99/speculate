cc6bfa568580b9bbdb5aa0b3e6cabe4e7e37731c
Nino Lopez <ninolopez91@gmail.com>
2025-09-07 12:32:06 -0400

    update

--- convex/convex.config.ts
+++ convex/convex.config.ts
@@ -1,5 +1,11 @@
 import { defineApp } from 'convex/server';
+import workpool from '@convex-dev/workpool/convex.config';
 
 const app = defineApp();
+
+// Configure workpool for TVDB syncing with appropriate parallelism limits
+app.use(workpool, {
+  name: 'tvdbSyncPool',
+});
 
 export default app;
